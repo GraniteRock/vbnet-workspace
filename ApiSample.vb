53e527f26820211b2b1f69fad5399a3e616b05f9
takuya（rock0x36） <yata.s15ste@gmail.com>
2017-08-27 00:01:38 +0900

    Update ApiSample.vb

--- ApiSample.vb
+++ ApiSample.vb
@@ -4,29 +4,28 @@ Imports DefaultNamespace
 Public Class ApiSample
 	Public Shared Sub Main()
 		' your code goes here
-        Dim apicaller = new ApiChildClass()
+		Dim apicaller = new ApiChildClass()
 
-        Dim result as String 
+		Dim result as String 
 
-        try
-            With apicaller
-                .BaseUri = "https://demo.sample.com/api"
-                .IsDebug = True 
-                .IsSSL = True 
+		try
+		    With apicaller
+			.BaseUri = "https://demo.sample.com/api"
+			.IsDebug = True 
+			.IsSSL = True 
 
-                // query param
-                .AddQueryParam("a","1")
-                .AddQueryParam("b","demo")
-                .AddQueryParam("c","100")
-                .AddQueryParam("type","xml")
+			// query param
+			.AddQueryParam("a","1")
+			.AddQueryParam("b","demo")
+			.AddQueryParam("c","100")
+			.AddQueryParam("type","xml")
 
-                result = Await .Calling()
-            End With
+			result = Await .Calling()
+		    End With
 
-        'いかなる例外もスローする
-        Catch ex As Exception
-            Debug.WriteLine(ex.message)
-        End Try
-
+		'いかなる例外もスローする
+		Catch ex As Exception
+		    Debug.WriteLine(ex.message)
+		End Try
 	End Sub
 End Class
